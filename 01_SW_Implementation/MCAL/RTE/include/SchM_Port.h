6ac3fcbfdcfa2af681a7d2432ed3bf650f57de37
Jason Pablo Jayme <jasonpj@fpt.com>
2025-01-09 16:01:08 +0800

    addition of MCAL

---
+++ 01_SW_Implementation/MCAL/RTE/include/SchM_Port.h
@@ -0,0 +1,172 @@
+/**
+*   @file    SchM_Port.h
+*   @version 1.0.4
+*
+*   @brief   AUTOSAR Rte - module interface
+*   @details This file contains the functions prototypes and data types of the AUTOSAR Rte.
+*            This file contains sample code only. It is not part of the production code deliverables.
+*
+*   @addtogroup RTE_MODULE
+*   @{
+*/
+/*==================================================================================================
+*   Project              : AUTOSAR 4.3 MCAL
+*   Platform             : ARM
+*   Peripheral           : 
+*   Dependencies         : none
+*
+*   Autosar Version      : 4.3.1
+*   Autosar Revision     : ASR_REL_4_3_REV_0001
+*   Autosar Conf.Variant :
+*   SW Version           : 1.0.4
+*   Build Version        : S32K1XX_MCAL_1_0_4_RTM_ASR_REL_4_3_REV_0001_04-Apr-22
+*
+*   (c) Copyright 2006-2016 Freescale Semiconductor, Inc. 
+*       Copyright 2017-2022 NXP
+*   All Rights Reserved.
+==================================================================================================*/
+/*==================================================================================================
+==================================================================================================*/
+
+#ifndef SCHM_PORT_H
+#define SCHM_PORT_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+/*==================================================================================================
+*                                         INCLUDE FILES
+* 1) system and project includes
+* 2) needed interfaces from external units
+* 3) internal and external interfaces from this unit
+==================================================================================================*/
+
+/*==================================================================================================
+*                               SOURCE FILE VERSION INFORMATION
+==================================================================================================*/
+#define SCHM_PORT_AR_RELEASE_MAJOR_VERSION     4
+#define SCHM_PORT_AR_RELEASE_MINOR_VERSION     3
+#define SCHM_PORT_AR_RELEASE_REVISION_VERSION  1
+#define SCHM_PORT_SW_MAJOR_VERSION             1
+#define SCHM_PORT_SW_MINOR_VERSION             0
+#define SCHM_PORT_SW_PATCH_VERSION             4
+
+/*==================================================================================================
+*                                      FILE VERSION CHECKS
+==================================================================================================*/
+
+
+/*==================================================================================================
+*                                           CONSTANTS
+==================================================================================================*/
+
+/*==================================================================================================
+*                                       DEFINES AND MACROS
+==================================================================================================*/
+
+/*==================================================================================================
+*                                             ENUMS
+==================================================================================================*/
+
+/*==================================================================================================
+*                                 STRUCTURES AND OTHER TYPEDEFS
+==================================================================================================*/
+
+/*==================================================================================================
+*                                 GLOBAL VARIABLE DECLARATIONS
+==================================================================================================*/
+
+/*==================================================================================================
+*                                     FUNCTION PROTOTYPES
+==================================================================================================*/
+#ifdef MCAL_TESTING_ENVIRONMENT
+/** 
+@brief   This function checks that all entered exclusive areas were also exited. 
+@details This function checks that all entered exclusive areas were also exited. The check
+         is done by verifying that all reentry_guard_* static variables are back to the
+         zero value.
+    
+@param[in]     void       No input parameters
+@return        void       This function does not return a value. Test asserts are used instead. 
+
+@pre  None
+@post None
+
+@remarks Covers 
+@remarks Implements 
+*/
+FUNC(void, RTE_CODE) SchM_Check_port(void);
+#endif /*MCAL_TESTING_ENVIRONMENT*/
+
+extern FUNC(void, RTE_CODE) SchM_Enter_Port_PORT_EXCLUSIVE_AREA_00(void);
+extern FUNC(void, RTE_CODE) SchM_Enter_Port_PORT_EXCLUSIVE_AREA_01(void);
+extern FUNC(void, RTE_CODE) SchM_Enter_Port_PORT_EXCLUSIVE_AREA_02(void);
+extern FUNC(void, RTE_CODE) SchM_Enter_Port_PORT_EXCLUSIVE_AREA_03(void);
+extern FUNC(void, RTE_CODE) SchM_Enter_Port_PORT_EXCLUSIVE_AREA_04(void);
+extern FUNC(void, RTE_CODE) SchM_Enter_Port_PORT_EXCLUSIVE_AREA_05(void);
+extern FUNC(void, RTE_CODE) SchM_Enter_Port_PORT_EXCLUSIVE_AREA_06(void);
+extern FUNC(void, RTE_CODE) SchM_Enter_Port_PORT_EXCLUSIVE_AREA_07(void);
+extern FUNC(void, RTE_CODE) SchM_Enter_Port_PORT_EXCLUSIVE_AREA_08(void);
+extern FUNC(void, RTE_CODE) SchM_Enter_Port_PORT_EXCLUSIVE_AREA_09(void);
+extern FUNC(void, RTE_CODE) SchM_Enter_Port_PORT_EXCLUSIVE_AREA_10(void);
+extern FUNC(void, RTE_CODE) SchM_Enter_Port_PORT_EXCLUSIVE_AREA_11(void);
+extern FUNC(void, RTE_CODE) SchM_Enter_Port_PORT_EXCLUSIVE_AREA_12(void);
+extern FUNC(void, RTE_CODE) SchM_Enter_Port_PORT_EXCLUSIVE_AREA_13(void);
+extern FUNC(void, RTE_CODE) SchM_Enter_Port_PORT_EXCLUSIVE_AREA_14(void);
+extern FUNC(void, RTE_CODE) SchM_Enter_Port_PORT_EXCLUSIVE_AREA_15(void);
+extern FUNC(void, RTE_CODE) SchM_Enter_Port_PORT_EXCLUSIVE_AREA_16(void);
+extern FUNC(void, RTE_CODE) SchM_Enter_Port_PORT_EXCLUSIVE_AREA_17(void);
+extern FUNC(void, RTE_CODE) SchM_Enter_Port_PORT_EXCLUSIVE_AREA_18(void);
+extern FUNC(void, RTE_CODE) SchM_Enter_Port_PORT_EXCLUSIVE_AREA_19(void);
+extern FUNC(void, RTE_CODE) SchM_Enter_Port_PORT_EXCLUSIVE_AREA_20(void);
+extern FUNC(void, RTE_CODE) SchM_Enter_Port_PORT_EXCLUSIVE_AREA_21(void);
+extern FUNC(void, RTE_CODE) SchM_Enter_Port_PORT_EXCLUSIVE_AREA_22(void);
+extern FUNC(void, RTE_CODE) SchM_Enter_Port_PORT_EXCLUSIVE_AREA_23(void);
+extern FUNC(void, RTE_CODE) SchM_Enter_Port_PORT_EXCLUSIVE_AREA_24(void);
+extern FUNC(void, RTE_CODE) SchM_Enter_Port_PORT_EXCLUSIVE_AREA_25(void);
+extern FUNC(void, RTE_CODE) SchM_Enter_Port_PORT_EXCLUSIVE_AREA_26(void);
+extern FUNC(void, RTE_CODE) SchM_Enter_Port_PORT_EXCLUSIVE_AREA_27(void);
+extern FUNC(void, RTE_CODE) SchM_Enter_Port_PORT_EXCLUSIVE_AREA_28(void);
+extern FUNC(void, RTE_CODE) SchM_Enter_Port_PORT_EXCLUSIVE_AREA_29(void);
+extern FUNC(void, RTE_CODE) SchM_Enter_Port_PORT_EXCLUSIVE_AREA_30(void);
+
+extern FUNC(void, RTE_CODE) SchM_Exit_Port_PORT_EXCLUSIVE_AREA_00(void);
+extern FUNC(void, RTE_CODE) SchM_Exit_Port_PORT_EXCLUSIVE_AREA_01(void);
+extern FUNC(void, RTE_CODE) SchM_Exit_Port_PORT_EXCLUSIVE_AREA_02(void);
+extern FUNC(void, RTE_CODE) SchM_Exit_Port_PORT_EXCLUSIVE_AREA_03(void);
+extern FUNC(void, RTE_CODE) SchM_Exit_Port_PORT_EXCLUSIVE_AREA_04(void);
+extern FUNC(void, RTE_CODE) SchM_Exit_Port_PORT_EXCLUSIVE_AREA_05(void);
+extern FUNC(void, RTE_CODE) SchM_Exit_Port_PORT_EXCLUSIVE_AREA_06(void);
+extern FUNC(void, RTE_CODE) SchM_Exit_Port_PORT_EXCLUSIVE_AREA_07(void);
+extern FUNC(void, RTE_CODE) SchM_Exit_Port_PORT_EXCLUSIVE_AREA_08(void);
+extern FUNC(void, RTE_CODE) SchM_Exit_Port_PORT_EXCLUSIVE_AREA_09(void);
+extern FUNC(void, RTE_CODE) SchM_Exit_Port_PORT_EXCLUSIVE_AREA_10(void);
+extern FUNC(void, RTE_CODE) SchM_Exit_Port_PORT_EXCLUSIVE_AREA_11(void);
+extern FUNC(void, RTE_CODE) SchM_Exit_Port_PORT_EXCLUSIVE_AREA_12(void);
+extern FUNC(void, RTE_CODE) SchM_Exit_Port_PORT_EXCLUSIVE_AREA_13(void);
+extern FUNC(void, RTE_CODE) SchM_Exit_Port_PORT_EXCLUSIVE_AREA_14(void);
+extern FUNC(void, RTE_CODE) SchM_Exit_Port_PORT_EXCLUSIVE_AREA_15(void);
+extern FUNC(void, RTE_CODE) SchM_Exit_Port_PORT_EXCLUSIVE_AREA_16(void);
+extern FUNC(void, RTE_CODE) SchM_Exit_Port_PORT_EXCLUSIVE_AREA_17(void);
+extern FUNC(void, RTE_CODE) SchM_Exit_Port_PORT_EXCLUSIVE_AREA_18(void);
+extern FUNC(void, RTE_CODE) SchM_Exit_Port_PORT_EXCLUSIVE_AREA_19(void);
+extern FUNC(void, RTE_CODE) SchM_Exit_Port_PORT_EXCLUSIVE_AREA_20(void);
+extern FUNC(void, RTE_CODE) SchM_Exit_Port_PORT_EXCLUSIVE_AREA_21(void);
+extern FUNC(void, RTE_CODE) SchM_Exit_Port_PORT_EXCLUSIVE_AREA_22(void);
+extern FUNC(void, RTE_CODE) SchM_Exit_Port_PORT_EXCLUSIVE_AREA_23(void);
+extern FUNC(void, RTE_CODE) SchM_Exit_Port_PORT_EXCLUSIVE_AREA_24(void);
+extern FUNC(void, RTE_CODE) SchM_Exit_Port_PORT_EXCLUSIVE_AREA_25(void);
+extern FUNC(void, RTE_CODE) SchM_Exit_Port_PORT_EXCLUSIVE_AREA_26(void);
+extern FUNC(void, RTE_CODE) SchM_Exit_Port_PORT_EXCLUSIVE_AREA_27(void);
+extern FUNC(void, RTE_CODE) SchM_Exit_Port_PORT_EXCLUSIVE_AREA_28(void);
+extern FUNC(void, RTE_CODE) SchM_Exit_Port_PORT_EXCLUSIVE_AREA_29(void);
+extern FUNC(void, RTE_CODE) SchM_Exit_Port_PORT_EXCLUSIVE_AREA_30(void);
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* SCHM_PORT_H */
+
+/** @} */
